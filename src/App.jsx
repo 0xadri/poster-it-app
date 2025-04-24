21219c5a13c5606c142504fbac350b950d30696b
0xadri <adiber_garcia@hotmail.com>
2025-04-24 19:05:39 +0200

    TAILWIND CSS setup + First Baby Steps

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Poster It App</h1>
-      </div>
-    </>
+    <div className="h-screen">
+      <header className="absolute top-0 w-full z-50 py-2 bg-indigo-900 px-15">
+        Musicians Patchwork App
+      </header>
+      <main className="h-full pt-15 px-15">
+        <h1 class="text-3xl font-bold">Let's get started!</h1>
+        <div></div>
+      </main>
+      <footer></footer>
+    </div>
   );
 }
 
