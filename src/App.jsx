c8abe2ba0159b95291364692c923618d9105022d
0xadri <adiber_garcia@hotmail.com>
2025-04-25 18:47:01 +0200

    Style Updates + Artist List + Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import "./App.css";
+import GenerateButton from "./components/GenerateButton";
+import NavBar from "./components/NavBar";
 import Poster from "./components/poster";
 import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <div className="h-screen">
-      <header className="absolute top-0 w-full z-50 py-2 bg-indigo-900 px-15">
-        Musicians Patchwork App
-      </header>
-      <main className="h-full pt-15 px-15">
-        <h1 className="text-8xl font-bold">Let's get started!</h1>
-        <div>
-          {/* Dev Tasks
+    <>
+      <div className="h-screen">
+        {/* <header className="absolute top-0 w-full z-50 py-2 bg-indigo-900 px-15">
+        Musicians Patchwork App .... d((-_-))b
+      </header> */}
+        <NavBar />
+        <main className="h-full pt-15 px-15">
+          <h1 className="text-8xl font-bold">Let's get started!</h1>
+          <div>
+            {/* PROJECT TASKS
+              Done:
+                - Poster Component Skeleton
+                - API Fetch: One Image Per Musician
               Prog:
-               - Poster Component Skeleton
-              Todo:
+                - API Fetch: Musicians List
+                - API Fetch: Images For Musician (From More Sources)
+               Todo:
                 - Pick Poster Format
                 - Pick Poster Size Horiz
                 - Pick Poster Size Vert
@@ -22,15 +30,15 @@ function App() {
                 - Pick Items Per Column
                 - Pick Musicians From List
                 - Pick Images From List
-                - API Fetch: Musicians List
-                - API Fetch: Images For Musician
            */}
-          <SearchBar />
-          <Poster />
-        </div>
-      </main>
-      <footer></footer>
-    </div>
+            <SearchBar />
+            <GenerateButton />
+            <Poster />
+          </div>
+        </main>
+        <footer></footer>
+      </div>
+    </>
   );
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,10 +25,21 @@ const SearchBar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Type Artist Name..." ref={searchInput} />
-      <button type="submit" className="ml-2">
-        Search
+    <form onSubmit={handleSubmit} className="my-6 text-2xl">
+      <input
+        name="search-artist"
+        type="text"
+        placeholder="Type Artist Name..."
+        ref={searchInput}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-50 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+      <button
+        type="submit"
+        className="ml-2 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+          SEARCH
+        </span>
       </button>
     </form>
   );
