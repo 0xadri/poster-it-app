fd8c9d25798afdd37f745e02068ae4ed98fdfadb
0xadri <adiber_garcia@hotmail.com>
2025-04-25 16:31:08 +0200

    SEARCH ARTIST: beta version

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,32 @@
 import { useRef } from "react";
-import { getArtist } from "/src/services/spotify-api";
+import { searchArtist } from "/src/services/spotify-api";
 
 const SearchBar = () => {
-  const searchTerm = useRef();
+  const searchInput = useRef(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const search = async () => {
-      try {
-        // const artists = await searchArtist(searchTerm);
-        const artistDetails = await getArtist("3PhoLpVuITZKcymswpck5b");
-        console.log(artistDetails);
-        // setMovies(artists);
-      } catch (e) {
-        console.log(e);
-        // setError("Failed To Search Artist");
-      } finally {
-        // setIsLoading(false);
-      }
-    };
-    search();
+    const searchQuery = searchInput.current.value;
+    try {
+      console.log(`Search "${searchQuery}"`);
+      const json = await searchArtist(searchQuery);
+      console.log(json);
+      const artistID = json.artists.items[0].id;
+      console.log(artistID);
+      const artistImgUrl = json.artists.items[0].images[0].url;
+      console.log(artistImgUrl);
+      // setMovies(artists);
+    } catch (e) {
+      console.log(e);
+      // setError("Failed To Search Artist");
+    } finally {
+      // setIsLoading(false);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Type Artist Name..." ref={searchTerm} />
+      <input type="text" placeholder="Type Artist Name..." ref={searchInput} />
       <button type="submit" className="ml-2">
         Search
       </button>
