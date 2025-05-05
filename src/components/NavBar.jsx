5479af237b8ec78fdd0a2aa9ef8e3dfdb8ebd692
0xadri <adiber_garcia@hotmail.com>
2025-05-05 19:59:29 +0200

    NAVBAR : RESPONSIVE

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,19 @@
 const NavBar = () => {
   return (
-    <nav className="px-15 bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="px-15 max-sm:px-1 bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/music-fan_00.png" className="h-8" alt="Flowbite Logo" />
+          <img
+            src="/music-fan_00.png"
+            className="max-h-8 max-sm:max-w-35"
+            alt="Flowbite Logo"
+          />
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <a
             href="https://github.com/0xadri/poster-it-app"
             target="_blank"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="max-sm:py-1 max-sm:px-2 max-sm:text-xs text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Code Repo
           </a>
