c1ffcabe5de3b7f4074bf818c26796f72fe0f19e
0xadri <adiber_garcia@hotmail.com>
2025-05-05 20:43:44 +0200

    RESPONSIVE : WARNING FOR MOBILES

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import NavBar from "./components/NavBar";
 import PosterPage from "./pages/PosterPage";
 import Play from "./pages/Play";
 import { Route, Routes } from "react-router";
+import { mustDisplayMobileWarning } from "./utils/miscUtils";
 
 function App() {
+  const showMobileWarning = mustDisplayMobileWarning();
+  if (showMobileWarning) {
+    alert(
+      "Hey YOU. We detected you are on a mobile device.\n\nYou should know this app's features are very limited on mobile devices.\n\nCheck it out from a laptop!"
+    );
+  }
+
   return (
     <>
       <div className="h-screen">
--- src/utils/miscUtils.js
+++ src/utils/miscUtils.js
@@ -30,3 +30,24 @@ export const getNextIndexInImageArray = (currentIndex, arrayLength) => {
   }
   return currentIndex;
 }
+
+
+export const mustDisplayMobileWarning = () => {
+  const WEB_STORAGE_WARNING = "mobile-warning";
+  const mobileWarningWasShown = sessionStorage.getItem(WEB_STORAGE_WARNING);
+  // if (mobileWarningWasShown) {
+  //   mobileWarningWasShown = JSON.parse(mobileWarningWasShown)
+  // }
+  const isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  if (mobileWarningWasShown){ // if message was displayed
+    return false;
+  }
+  else if (!isMobile){ // else if (is desktop or tablet)
+    return false;
+  }
+  else if (isMobile && !mobileWarningWasShown){ // else if (is mobile and was not displayed)
+    sessionStorage.setItem(WEB_STORAGE_WARNING, JSON.stringify(true));
+    return true;
+  }
+}
